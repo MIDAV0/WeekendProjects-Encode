8c0501c34aa4f4d58ba1fa9161a9af8b587884f8
Vadzim Mahilny <magilnyy.vadimo@gmail.com>
2023-05-23 17:48:23 +0100

    Refactor Vote

--- Weekend Project 4/frontend/tokenizedBallot/frontend/components/bodyComponents/Vote.jsx
+++ Weekend Project 4/frontend/tokenizedBallot/frontend/components/bodyComponents/Vote.jsx
@@ -3,16 +3,17 @@ import getDataFromAPI from "../utils/fetcher";
 
 export default function Vote({ signer }) {
 	const [isLoading, setLoading] = useState(false);
+	const [isProposalLoading, setProposalLoading] = useState(false);
 	const [proposals, setProposals] = useState([]);
 	const [proposalId, setProposalId] = useState(null);
 
 	useEffect(() => {
-		fetch('http://localhost:3001/get-proposals')
-		.then((res) => res.json())
-		.then((data) => {
-			setProposals(data);
-			setLoading(false);
-		})
+		getDataFromAPI(
+			"GET", 
+			{}, 
+			"http://localhost:3001/get-proposals", 
+			setProposalLoading, 
+			setProposals)
 	}, [])
 
 
@@ -20,10 +21,12 @@ export default function Vote({ signer }) {
 		<>
 			<h1>Vote</h1>
 
-			{ proposals.map((index, proposal) => (
-					<div>
-						<p>{proposal} : {index}</p>
-					</div>
+			{ isProposalLoading ? <p>Loading proposals ...</p> :
+				proposals.length === 0 ? <p>No proposals</p> :
+					proposals.map((index, proposal) => (
+							<div>
+								<p>{proposal} : {index}</p>
+							</div>
 			))}
 			<input type="number" placeholder="Proposal ID" onChange={(e) => setProposalId(e.target.value)}></input>
 			<button
